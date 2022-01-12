f46a91dc957a7eb5d9c5bb1e8b42d7b45dac86ca
Erich Hils <mrerichhils@gmail.com>
2022-01-12 13:51:45 -0600

    finish chapter one

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const fillGasTank = (gallons) => {
-    if (gallons > 15) {
-    console.log(`I filled the tank with ${gallons} of gas`)
+    if (gallons <= 15) {
+        console.log(`I filled the tank with ${gallons} of gas`)    
     }
+    else console.log(`you cannot add this much gas to the tank`)
+    
 }
 
 fillGasTank(17)
+fillGasTank(13)
